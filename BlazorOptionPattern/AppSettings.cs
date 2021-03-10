f63197406af5dee34690bac54d354bbd43b6bd26
k-green-panda <stvcx5c@gmail.com>
2021-03-10 13:44:24 +0900

    コミット

---
+++ BlazorOptionPattern/AppSettings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorOptionPattern
+{
+    public class MySettings
+    {
+        public string StringSetting { get; set; }
+        public int IntSetting { get; set; }
+        public List<string> ListSetting { get; set; }
+        public Dictionary<string, string> DictionarySetting { get; set; }
+        public FruitEnum EnumSetting { get; set; }
+
+        // セッターが未定義 or privateの場合はバインドできずnullになる
+        public string NonSetter { get; }
+        public string NonPublicSetter { get; private set; }
+    }
+
+    public enum FruitEnum
+    {
+        Apple,
+        Orange,
+        Meron,
+        Peach
+    }
+}
---
+++ BlazorOptionPattern/Data/WeatherForecastService.cs
@@ -0,0 +1,33 @@
+using Microsoft.Extensions.Options;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorOptionPattern.Data
+{
+    public class WeatherForecastService
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly MySettings _options;
+
+        // オプションクラス受け取り
+        public WeatherForecastService(IOptionsMonitor<MySettings> options)
+        {
+            _options = options.CurrentValue;
+        }
+        public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
+        {
+            var rng = new Random();
+            return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = startDate.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            }).ToArray());
+        }
+    }
+}
